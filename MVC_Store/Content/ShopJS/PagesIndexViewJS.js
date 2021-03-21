82fc60e280fba540970c49e0cf5b087516cd1f95
евгений ковпак <evgenij-kowpak@outlook.com>
2021-03-21 21:21:33 +0200

    online shopping on asp.net mvc5
    My test project on asp.net framework mvc5

---
+++ MVC_Store/Content/ShopJS/PagesIndexViewJS.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+
+    $("a.delete").click(function () {
+        if (!confirm("Confirm page deletion")) return false;
+    });
+
+
+    $("table#pages tbody").sortable({
+        items: "tr:not(.home)",
+        placeholder: "ui-state-highlight",
+        update: function () {
+            var ids = $("table#pages tbody").sortable("serialize");
+            //console.log(ids);
+            var url = "/Admin/Pages/ReorderPages";
+
+            $.post(url, ids, function (data) {
+            });
+        }
+    });
+});
---
+++ MVC_Store/Content/ShopJS/ProductsScript.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+
+    $("#SelectCategory").on("change", function () {
+        var url = $(this).val();
+
+        if (url) {
+            window.location = "/admin/shop/Products?catId=" + url;
+        }
+        return false;
+    });
+
+
+    $("a.delete").click(function () {
+        if (!confirm("Confirm product deletion")) return false;
+    });
+
+});
